86b119f7001249ab9524bb49f7c3883332d2a53c
Arthur <aginiatulllin@gmail.com>
2022-11-11 17:38:21 +0300

    Добавлена авторизация и логирование.

--- modernschool_back/AuthorizationAttr/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace modernschool_back.AuthorizationAttr
-{
-    public class User
-    {
-        public int ID { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}
