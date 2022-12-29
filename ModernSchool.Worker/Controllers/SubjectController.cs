75717b57ec7658c7a8b6f09d5a5eae7f6dc13490
exdeleter <exdeleter@gmail.com>
2022-12-29 15:48:31 +0300

    добавил базовый контроллер

--- ModernSchool.Worker/Controllers/SubjectController.cs
+++ ModernSchool.Worker/Controllers/SubjectController.cs
@@ -1,90 +1,88 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ModernSchool.Worker.Contexts;
 using ModernSchool.Worker.Models;
 
-namespace ModernSchool.Worker.Controllers
+namespace ModernSchool.Worker.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class SubjectController : ControllerBase
 {
-    [Authorize]
-    [Route("api/[controller]")]
-    [ApiController]
-    public class SubjectController : ControllerBase
+    SchoolDBContext db;
+    public SubjectController(SchoolDBContext db)
     {
-        SchoolDBContext db;
-        public SubjectController(SchoolDBContext db)
+        this.db = db;
+        if (!db.Subjects.Any())
         {
-            this.db = db;
-            if (!db.Subjects.Any())
-            {
-                db.Subjects.Add(new Subject { Name = "Философия" });
-                db.Subjects.Add(new Subject { Name = "УМФ" });
-                db.Subjects.Add(new Subject { Name = "Русский язык" });
-                db.Subjects.Add(new Subject { Name = "Математика" });
-                db.Subjects.Add(new Subject { Name = "Обществознание" });
-                db.Subjects.Add(new Subject { Name = "Физкультура" });
-                db.SaveChanges();
-            }
+            db.Subjects.Add(new Subject { Name = "Философия" });
+            db.Subjects.Add(new Subject { Name = "УМФ" });
+            db.Subjects.Add(new Subject { Name = "Русский язык" });
+            db.Subjects.Add(new Subject { Name = "Математика" });
+            db.Subjects.Add(new Subject { Name = "Обществознание" });
+            db.Subjects.Add(new Subject { Name = "Физкультура" });
+            db.SaveChanges();
         }
+    }
         
 
-        [HttpGet(Name = "GetAllSubjects"), Authorize(Roles = "Admin")]
-        public async Task<ActionResult<IEnumerable<Subject>>> Get()
-        {
-            return await db.Subjects.ToListAsync();
-        }
+    [HttpGet(Name = "GetAllSubjects"), Authorize(Roles = "Admin")]
+    public async Task<ActionResult<IEnumerable<Subject>>> Get()
+    {
+        return await db.Subjects.ToListAsync();
+    }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Subject>>Get(int id)
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Subject>>Get(int id)
+    {
+        Subject subject = await db.Subjects.FirstOrDefaultAsync(x => x.Id == id);
+        if (subject == null) 
         {
-            Subject subject = await db.Subjects.FirstOrDefaultAsync(x => x.Id == id);
-            if (subject == null) 
-            {
-                return NotFound();
-            }
-            return new ObjectResult(subject);
+            return NotFound();
         }
+        return new ObjectResult(subject);
+    }
 
-        [HttpPost]
-        public async Task<ActionResult<Subject>>Post(Subject subject)
+    [HttpPost]
+    public async Task<ActionResult<Subject>>Post(Subject subject)
+    {
+        if (subject == null) 
         {
-            if (subject == null) 
-            {
-                return BadRequest();
-            }
-            db.Subjects.Add(subject);
-            await db.SaveChangesAsync();
-            return Ok(subject);
+            return BadRequest();
         }
+        db.Subjects.Add(subject);
+        await db.SaveChangesAsync();
+        return Ok(subject);
+    }
 
-        [HttpPut]
-        public async Task<ActionResult<Subject>> Put(Subject subject)
+    [HttpPut]
+    public async Task<ActionResult<Subject>> Put(Subject subject)
+    {
+        if (subject == null)
         {
-            if (subject == null)
-            {
-                return BadRequest();
-            }
-            if (!db.Subjects.Any(x => x.Id == subject.Id))
-            {
-                return NotFound();
-            }
-            db.Update(subject);
-            await db.SaveChangesAsync();
-            return Ok(subject);
+            return BadRequest();
         }
+        if (!db.Subjects.Any(x => x.Id == subject.Id))
+        {
+            return NotFound();
+        }
+        db.Update(subject);
+        await db.SaveChangesAsync();
+        return Ok(subject);
+    }
 
-        [HttpDelete]
-        public async Task<ActionResult<Subject>> Delete(int id)
+    [HttpDelete]
+    public async Task<ActionResult<Subject>> Delete(int id)
+    {
+        Subject subject = db.Subjects.FirstOrDefault(x => x.Id == id);
+        if (subject == null)
         {
-            Subject subject = db.Subjects.FirstOrDefault(x => x.Id == id);
-            if (subject == null)
-            {
-                return NotFound();
-            }
-            db.Subjects.Remove(subject);
-            await db.SaveChangesAsync();
-            return Ok(subject);
+            return NotFound();
         }
+        db.Subjects.Remove(subject);
+        await db.SaveChangesAsync();
+        return Ok(subject);
     }
 }
---
+++ ModernSchool.Worker/Interfaces/IBaseService.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ModernSchool.Worker.Models;
+
+namespace ModernSchool.Worker.Interfaces;
+
+public interface IBaseService<T>
+    where T : BaseEntity
+{
+    Task<ActionResult<IEnumerable<T>>> Get();
+    
+    Task<ActionResult<T>> Get(int id);
+    
+    Task<ActionResult<T>> Post(T? entity);
+    
+    Task<ActionResult<T>> Put (T entity);
+    
+    Task<ActionResult<T>> Delete(int id);
+}
